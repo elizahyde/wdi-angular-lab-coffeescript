59b29fb0df2f053dce2e1a4fc146767de1d71a21
elizahyde <elizabeth@elizahyde.com>
2014-03-10 18:10:59 -0700

    Search, sort, and add works

--- app/books_controller.js
+++ app/books_controller.js
@@ -6,6 +6,21 @@ bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
 
+  $scope.cart = [];
+
   //Code to manage cart goes here
+
+  // Add book to cart
+  $scope.addItem = function( book ) {
+    $scope.cart.push(book);
+  };
+
+  $scope.totalCartValue = function() {
+     // Insert math here
+  }
+
+  $scope.emptyCart = function() {
+    // Empty the cart
+  }
 
 };
